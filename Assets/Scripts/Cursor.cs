1e986ca83a8f4557996c8ef95236f4423aa9f2e0
arata <arata04962@gmail.com>
2019-08-10 20:55:32 +0900

    :sparkles: カーソル

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.normal.y > 0.9f) {
+                Vector3 hitPos = hit.transform.position;
+                hitPos.y += 0.5f;
+                this.transform.position = hitPos; 
+            }
+        }
+    }
+}
